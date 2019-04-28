9eee41fc99efa1d440856a033a4eb76f24691a9b
Viliam Kopecky <vk@viliamkopecky.cz>
2019-04-28 15:53:26 +0200

    support prefilled credentials; Closes #36

--- docker/nginx/entrypoint.sh
+++ docker/nginx/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
 
 sed "s~REACT_APP_API_URL~$API_URL~" /src/index.html.raw > /src/index.html
+sed "s~REACT_APP_PREFILLED_CREDENTIALS~$PREFILLED_CREDENTIALS~" /src/index.html.raw > /src/index.html
 nginx -g "daemon off;"
--- src/globals.d.ts
+++ src/globals.d.ts
@@ -4,6 +4,7 @@ import { EinstoreApp } from './App'
 declare global {
 	interface Window {
 		API_URL: string
+		PREFILLED_CREDENTIALS: string
 		rootApp?: EinstoreApp
 		Einstore: Einstore
 		logout: () => void
