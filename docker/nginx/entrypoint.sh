3cdbc19ffaa07a92d86c1450944bc722e1e75834
Viliam Kopecky <vk@viliamkopecky.cz>
2019-04-29 12:44:29 +0200

    hotfix env variable

--- docker/nginx/entrypoint.sh
+++ docker/nginx/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
-sed "s~REACT_APP_API_URL~$API_URL~" /src/index.html.raw > /src/index.html
+sed "s~REACT_APP_API_URL~$API_URL~" /src/index.html.raw | sed "s~REACT_APP_PREFILLED_CREDENTIALS~$PREFILLED_CREDENTIALS~" > /src/index.html
 nginx -g "daemon off;"
