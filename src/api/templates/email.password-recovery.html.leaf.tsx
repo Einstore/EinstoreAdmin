446d6ac6db9c7c7406be01e25a90a9cf891f105e
Viliam Kopecky <vk@viliamkopecky.cz>
2019-05-16 15:46:04 +0200

    Typo

--- src/api/templates/email.password-recovery.html.leaf.tsx
+++ src/api/templates/email.password-recovery.html.leaf.tsx
@@ -4,7 +4,7 @@ import htmlEmailButton from './parts/htmlEmailButton'
 export default function emailInvitationHtmlLeaf(params: any) {
 	const content = /*html*/ `
 <p style="font-size:1.25em;font-weight:bold">Hi #(user.firstname) #(user.lastname)</p>
-<p>Please confirm your email #(user.email) by clicking on this button./p>
+<p>Please confirm your email #(user.email) by clicking on this button.</p>
 ${htmlEmailButton({ label: 'Confirm your email', href: '#(link)', center: true }, params)}
 `
 
