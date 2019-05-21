b705f00ff8cab95f8121b5f2646c54c1a00c9bc2
Viliam Kopecky <vk@viliamkopecky.cz>
2019-05-21 16:56:32 +0200

    Gravatar using Squircle

--- src/components/Gravatar.tsx
+++ src/components/Gravatar.tsx
@@ -2,6 +2,7 @@ import cn from 'classnames'
 import CryptoJS from 'crypto-js'
 import React from 'react'
 import './gravatar.sass'
+import Squircle, { SquircleRadius } from 'ui/Squircle'
 
 interface GravatarProps {
 	email?: string
@@ -16,8 +17,8 @@ export default class Gravatar extends React.PureComponent<GravatarProps> {
 		const hash = CryptoJS.MD5(this.props.email)
 		return (
 			<div className={cn('gravatar', this.props.className)}>
-				<img
-					alt=""
+				<Squircle
+					radius={SquircleRadius.L}
 					src={`https://www.gravatar.com/avatar/${hash}?s=66`}
 					srcSet={`https://www.gravatar.com/avatar/${hash}?s=66, https://www.gravatar.com/avatar/${hash}?s=99 1.5x, https://www.gravatar.com/avatar/${hash}?s=132 2x`}
 				/>
