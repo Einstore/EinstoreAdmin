1c655d4b6f386a27979c6bf49a64eb24a4a60187
Viliam Kopecky <vk@viliamkopecky.cz>
2019-05-21 17:01:00 +0200

    Team icons using Squircle

--- src/components/TeamTag.tsx
+++ src/components/TeamTag.tsx
@@ -1,63 +1,37 @@
 import React from 'react'
-import allTeamsIcon from '../shapes/all-teams.png'
+import allTeamsIcon from '../ui/Icons/all-teams.svg'
+import addTeamIcon from '../ui/Icons/add-team.svg'
 import AppIcon from './AppIcon'
 import './teamIcon.sass'
-import getContrastColor from '../utils/getContrastColor'
+import Squircle from 'ui/Squircle'
 
 export const TeamIcon = ({ team, iconSize }: { team: any; iconSize?: number }) => {
-	if (!team) {
-		return (
-			<span className="teamTag-img" style={{ fontSize: iconSize }}>
-				<div
-					className="teamIcon"
-					style={{ backgroundColor: `#eee`, color: `${getContrastColor('eee')}` }}
-				>
-					<div className="teamIcon-initials"></div>
-				</div>
-			</span>
-		)
+	let icon = <Squircle color={'#eee'} />
+
+	if (team) {
+		if (team.id === 'new') {
+			icon = <Squircle color={'#236aea'} src={addTeamIcon} />
+		} else if (team.id === 'all') {
+			icon = <Squircle color={'#0AB'} src={allTeamsIcon} />
+		} else if (team.icon) {
+			icon = <AppIcon
+				context="teams"
+				id={team.id}
+				name={team.initials}
+			/>
+		} else {
+			icon = <Squircle color={team.color} placeholder={team.initials} />
+		}
+	} else {
+		icon = <Squircle color={'#eee'} />
 	}
 
-	if (team.id === 'new') {
-		return (
-			<span className="teamTag-img" style={{ fontSize: iconSize }}>
-				<div className="teamIcon view-add">
-					<div className="teamIcon-initials">+</div>
-				</div>
-			</span>
-		)
-	}
-
-	if (team.id === 'all') {
-		return (
-			<span className="teamTag-img" style={{ fontSize: iconSize }}>
-				<div className="teamIcon" style={{ backgroundColor: `#${'0AB'}` }}>
-					<div
-						className="teamIcon-initials"
-						style={{ backgroundImage: `url('${allTeamsIcon}')` }}
-					/>
-				</div>
-			</span>
-		)
-	}
-	return team.icon ? (
-		<AppIcon
-			iconSize={iconSize}
-			context="teams"
-			id={team.id}
-			className="teamTag-img"
-			name={team.initials}
-		/>
-	) : (
+	return (
 		<span className="teamTag-img" style={{ fontSize: iconSize }}>
-			<div
-				className="teamIcon"
-				style={{ backgroundColor: `#${team.color}`, color: `${getContrastColor(team.color)}` }}
-			>
-				<div className="teamIcon-initials">{team.initials}</div>
-			</div>
+			{icon}
 		</span>
 	)
+
 }
 
 export const TeamTag = ({
