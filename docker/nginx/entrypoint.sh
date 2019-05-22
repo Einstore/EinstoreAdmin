6e799540a1025bc7e7db5f98c22e66f476e756e3
Viliam Kopecky <vk@viliamkopecky.cz>
2019-05-22 12:53:50 +0200

    Fix typo of filenames

--- docker/nginx/entrypoint.sh
+++ docker/nginx/entrypoint.sh
@@ -4,8 +4,6 @@ sed \
 	-e "s~%REACT_APP_API_URL%~$API_URL~g" \
 	-e "s~%REACT_APP_PREFILLED_CREDENTIALS%~$PREFILLED_CREDENTIALS~g" \
 	-e "s~%REACT_APP_SENTRY_DSN%~$SENTRY_DSN~g" \
-	/src/index.html > /src/index.html.raw
-
-mv /src/index.html.raw /src/index.html
+	/src/index.html.raw > /src/index.html
 
 nginx -g "daemon off;"
