f3ab73332fb64256041dd8dc7a664195d30863d7
Viliam Kopecky <vk@viliamkopecky.cz>
2019-05-22 11:59:41 +0200

    Filling env variables during deploy time

--- docker/nginx/entrypoint.sh
+++ docker/nginx/entrypoint.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env sh
 
-sed "s~REACT_APP_API_URL~$API_URL~" /src/index.html.raw | sed "s~REACT_APP_PREFILLED_CREDENTIALS~$PREFILLED_CREDENTIALS~" > /src/index.html
+sed \
+	-e "s~%REACT_APP_API_URL%~$API_URL~g" \
+	-e "s~%REACT_APP_PREFILLED_CREDENTIALS%~$PREFILLED_CREDENTIALS~g" \
+	-e "s~%REACT_APP_SENTRY_DSN%~$SENTRY_DSN~g" \
+	/src/index.html > /src/index.html.raw
+
+mv /src/index.html.raw /src/index.html
+
 nginx -g "daemon off;"
